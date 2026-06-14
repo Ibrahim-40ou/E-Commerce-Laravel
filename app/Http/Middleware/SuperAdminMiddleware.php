da3b719523476576e2991efc6f4eeca012c4b22e
Ibrahim Haitham <133378045+Ibrahim-40ou@users.noreply.github.com>
2026-06-14 11:13:17 +0300

    fixes to middleware

--- app/Http/Middleware/SuperAdminMiddleware.php
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-use function Pest\Laravel\json;
-
 class SuperAdminMiddleware
 {
     /**
@@ -18,7 +16,7 @@ class SuperAdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::user()->role !== 'super_admin') {
+        if (Auth::user()?->role !== 'super_admin') {
             return response()->json(['message' => 'Forbidden.'], 403);
         }
 
